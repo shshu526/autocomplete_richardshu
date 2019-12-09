300763141681a75f1e5689d373a4032a93e5df41
shshu526 <53555825+shshu526@users.noreply.github.com>
2019-12-09 15:56:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'autocomplete_richardshu',         # How you named your package folder (MyLib)
   packages = ['autocomplete_richardshu'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'An autocomplete program that gives suggestions',   # Give a short description about your library
   author = 'Richard Shu',                   # Type in your name
